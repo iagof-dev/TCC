19a889b6bcec45013983690ecc7c029885f07342
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2024-04-03 15:36:47 -0300

    app: started autocomplete function/style

--- APP/app-library/src/pages/Login.js
+++ APP/app-library/src/pages/Login.js
@@ -77,7 +77,7 @@ export default function Login(props) {
 				<form className="flex flex-col items-center gap-3" onSubmit={handleLoginSubmit}>
 					<span className="gap-5">
 						<label className="mr-10 text-lg">RM:</label>
-						<input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-70 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" placeholder="000000" pattern="[0-9]*" onChange={handleRMChange} onKeyDown={onKeyDownRM} value={RM} maxLength={"6"} type="text" />
+						<input required className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-70 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" placeholder="000000" pattern="[0-9]*" onChange={handleRMChange} onKeyDown={onKeyDownRM} value={RM} maxLength={"6"} type="text" />
 					</span>
 
 					<button className="button no-wrap align-center mx-2 w-full py-2 px-4 rounded text-lg" type="submit">
--- APP/app-library/src/pages/Search.js
+++ APP/app-library/src/pages/Search.js
@@ -1,8 +1,91 @@
+import { useState } from 'react'
+import Autocomplete from '@mui/material/Autocomplete';
+import { TextField, createTheme } from '@mui/material'
+import Chip from '@mui/material/Chip';
+
 export default function Search(props) {
-	const { setPath, path} = { ...props }
-	return(
-		<h1>
-			Search
-		</h1>
+	const { setPath, path } = { ...props }
+
+	const genresAndCourses = ["Administração", "Informática", "Nutrição"]
+
+
+	const theme = createTheme({
+		typography: [
+			"Figtree"
+		].join(",")
+	})
+
+
+	return (
+		<>
+
+			<h1 className="pb-5 text-3xl">
+				Pesquisa de Livro
+			</h1>
+			<form>
+				<p className="p-hint">
+					Pesquise por
+				</p>
+				<span class="flex gap-7 w-full items-center">
+					<label className="input-label">
+						Título
+					</label>
+					<input required placeholder="Título do Livro" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-[50vw] py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" />
+				</span>
+				<p className="p-hint">
+					ou
+				</p>
+				<span class="flex gap-7 w-full items-center">
+					<label className="input-label">
+						Autor
+					</label>
+					<input required placeholder="Nome do autor" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-[50vw] py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400" />
+				</span>
+				<p className="p-hint">
+					ou
+				</p>
+				<span className="flex gap-7 w-full items-center">
+
+					<label className="input-label">
+						Gênero/curso
+					</label>
+
+					{/* <Autocomplete
+						disablePortal
+						id="tags-filled"
+						options={genresAndCourses}
+						bgcolor="red"
+						freeSolo
+						sx={{ width: 300 }}
+						renderInput={(params) => <TextField {...params} label="" />}
+					/> */}
+
+					<Autocomplete
+						multiple
+						id="tags-filled"
+						fullWidth
+						options={genresAndCourses}
+						freeSolo
+						renderTags={(value, getTagProps) =>
+							value.map((option, index) => (
+								<Chip variant="outlined" label={option} {...getTagProps({ index })} />
+							))
+						}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								variant="filled"
+								placeholder="Gêneros ou cursos"
+								className=' bg-gray-200 appearance-none border-2 border-gray-200 rounded w-[50vw] py-none px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-400 autocomplete'
+							/>
+						)}
+					/>
+
+				</span>
+			</form>
+
+
+		</>
+
 	)
 }
