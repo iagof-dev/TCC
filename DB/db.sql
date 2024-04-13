d278aeed7c464a1406d871bd269a0e6214a1c8a3
Math H. Ferreira <101345181+MathFerreiraDev@users.noreply.github.com>
2024-04-13 18:34:58 -0300

    Update db.sql

--- DB/db.sql
+++ DB/db.sql
@@ -69,7 +69,7 @@ CREATE TABLE
         data_aluguel DATE NOT NULL,
         data_devolucao DATE NOT NULL,
         status_livro VARCHAR(20) NOT NULL, -- PENDENTE, AO DEVOLVER, DEVOLVIDO
-        prazo int not null
+        prazo int NOT NULL
     );
 
 CREATE TABLE
