d5963b9acd97e5e0dae26d5104c624a3177e7ed2
Iago F. <n3rdydzn@hotmail.com>
2024-08-22 21:50:18 -0300

    fix: readme

--- APP/app-library/README.md
+++ APP/app-library/README.md
@@ -1,8 +1,5 @@
-# Getting Started with creating an electron react app. 
-This project was generated from https://github.com/soulehshaikh99/create-react-electron-app 
+Comandos para compilar:
 
-This version is updated and fixed so that there wouldn't be any configuration issues.
+npm install
 
-It contains only the bare minimum dependencies for an electron react app. 
-
-[No typescript was used, only javascript]
+npm run electron-dev
--- APP/app-library/src/App.js
+++ APP/app-library/src/App.js
@@ -21,64 +21,64 @@ function App() {
 	const [librarianId, setLibrarianId] = useState(-1)
 
 	//Student Application
-	// return (
-	// 	<BrowserRouter>
-
-
-	// 		<Routes>
-	// 			<Route path='/' element={<StudentLogin setPath={setPath} userInfo={userInfo} setUserInfo={setUserInfo} />} />
-
-	// 			<Route path='/studentMenu' element={<StudentLayout setPath={setPath} path={path}  userInfo={userInfo} setUserInfo={setUserInfo}/>}>
-
-	// 				<Route path="/studentMenu/studentList" element={<List setPath={setPath} path={path} userInfo={userInfo} setUserInfo={setUserInfo}/>}/>
-
-	// 				<Route path="/studentMenu/studentSearch" element={<Search setPath={setPath} path={path} userInfo={userInfo} setUserInfo={setUserInfo}/>}/>
-
-	// 			</Route>
-
-	// 		</Routes>
-
-	// 	</BrowserRouter>
-	// );
-
-
-	//Librarian Application
 	return (
 		<BrowserRouter>
 
+
 			<Routes>
+				<Route path='/' element={<StudentLogin setPath={setPath} userInfo={userInfo} setUserInfo={setUserInfo} />} />
 
-				<Route path="/" element={<LibrarianLogin setPath={setPath} librarian={librarian} setLibrarian={setLibrarian} setLibrarianId={setLibrarianId} />} />
+				<Route path='/studentMenu' element={<StudentLayout setPath={setPath} path={path}  userInfo={userInfo} setUserInfo={setUserInfo}/>}>
 
-				<Route path="/librarianMenu" element={<LibrarianMenu setPath={setPath} librarian={librarian} />}  >
+					<Route path="/studentMenu/studentList" element={<List setPath={setPath} path={path} userInfo={userInfo} setUserInfo={setUserInfo}/>}/>
 
-					{/* Empréstimo/devolução */}
-
-					<Route path="/librarianMenu/librarianLoan"
-						element={<LibrarianLoan librarianId={librarianId} />} />
-
-					{/* Listar leituras dos alunos */}
-
-					< Route path="/librarianMenu/librarianList"
-						element={<LibrarianList librarianId={librarianId} />} />
-
-
-					{/* Adicionar livro ao sistema */}
-
-					<Route path="/librarianMenu/librarianAdd"
-						element={<LibrarianAdd librarianId={librarianId} />} />
-
-					{/* Editar/remover livro do sistema */}
-
-					<Route path="/librarianMenu/librarianEdit"
-						element={<LibrarianEdit librarianId={librarianId} />} />
+					<Route path="/studentMenu/studentSearch" element={<Search setPath={setPath} path={path} userInfo={userInfo} setUserInfo={setUserInfo}/>}/>
 
 				</Route>
 
 			</Routes>
 
 		</BrowserRouter>
-	)
+	);
+
+
+	//Librarian Application
+	// return (
+	// 	<BrowserRouter>
+
+	// 		<Routes>
+
+	// 			<Route path="/" element={<LibrarianLogin setPath={setPath} librarian={librarian} setLibrarian={setLibrarian} setLibrarianId={setLibrarianId} />} />
+
+	// 			<Route path="/librarianMenu" element={<LibrarianMenu setPath={setPath} librarian={librarian} />}  >
+
+	// 				{/* Empréstimo/devolução */}
+
+	// 				<Route path="/librarianMenu/librarianLoan"
+	// 					element={<LibrarianLoan librarianId={librarianId} />} />
+
+	// 				{/* Listar leituras dos alunos */}
+
+	// 				< Route path="/librarianMenu/librarianList"
+	// 					element={<LibrarianList librarianId={librarianId} />} />
+
+
+	// 				{/* Adicionar livro ao sistema */}
+
+	// 				<Route path="/librarianMenu/librarianAdd"
+	// 					element={<LibrarianAdd librarianId={librarianId} />} />
+
+	// 				{/* Editar/remover livro do sistema */}
+
+	// 				<Route path="/librarianMenu/librarianEdit"
+	// 					element={<LibrarianEdit librarianId={librarianId} />} />
+
+	// 			</Route>
+
+	// 		</Routes>
+
+	// 	</BrowserRouter>
+	// )
 }
 
 export default App;
