0fcf402866befb73b2e3c6327818bcefbe74e270
Iago F. <n3rdydzn@hotmail.com>
2024-10-03 19:33:48 -0300

    add: remove method generos_livros

--- API/classes/secret.php
+++ API/classes/secret.php
@@ -4,10 +4,10 @@ class config{
   private $informations = [
     "db_ip" => "0.0.0.0", //colocar "mysql" de volta dps
     "db_port" => "3306",
-    "db_user" => "",
-    "db_pass" => "",
+    "db_user" => "n3rdydev",
+    "db_pass" => "N3rdygamerbr@123",
     "db_name" => "tccapi",
-    "imagesApiKey" => ""
+    "imagesApiKey" => "e904bc960e7c33800f6de0c7cd9d1fbf1cbfa94a20459551c20a45c6ba52e830"
   ];
 
   function get(){
